a3821f58e4be2f7d4bba0c2eba27937ec1193465
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-05-08 21:01:54 +0300

    Added info section

---
+++ js/info-slick-script.js
@@ -0,0 +1,23 @@
+jQuery(function () {
+    $('.info').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        mobileFirst: true,
+        responsive: [
+            {
+                    breakpoint: 786,
+                    settings: 'unslick'
+            }
+        ],
+        speed: 1000,
+        // easing: 'ease',
+        // autoplay: true,
+        // autoplaySpeed: 30000,
+        // pauseOnFocus:false,
+        // pauseOnHover: false,
+        // pauseOnDotsHover:true,
+    });
+});
