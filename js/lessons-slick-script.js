e5a8323cbe3f60e832ea8b2c3155ba0803bb6d60
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-05-09 14:02:17 +0300

    Added lessons section

---
+++ js/lessons-slick-script.js
@@ -0,0 +1,34 @@
+jQuery(function () {
+    $('.lessons').slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 786,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 1440,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                    slidesToShow: 2,
+                }
+            }
+        ],
+        speed: 1000,
+        // easing: 'ease',
+        // autoplay: true,
+        // autoplaySpeed: 30000,
+        // pauseOnFocus:false,
+        // pauseOnHover: false,
+        // pauseOnDotsHover:true,
+    });
+});
