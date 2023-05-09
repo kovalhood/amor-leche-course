19b64e6522bb70b70e1e05b895bb7af91e028d6b
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-05-09 14:37:05 +0300

    Added reviews section

---
+++ js/reviews-slick-script.js
@@ -0,0 +1,11 @@
+jQuery(function () {
+    $('.reviews-mobile__wrapper').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        mobileFirst: true,
+        speed: 1000,
+    });
+});
