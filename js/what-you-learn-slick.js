c4c743ef8c3b1fb63e44ac3a2f731a1b091edea6
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-05-10 01:23:36 +0300

    Fixed issues and added content

---
+++ js/what-you-learn-slick.js
@@ -0,0 +1,13 @@
+$('.what-you-learn__images').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnFocus:false,
+    pauseOnHover: false,
+    pauseOnDotsHover:false
+});
